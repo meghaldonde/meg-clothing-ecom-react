7f2208dd06b732309cf7b02d04d874bb71e2ead7
meghaldonde <meghal.donde@gmail.com>
2020-09-20 22:59:22 -0400

    add form-input and custom-button components and styling

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import SignInAndSignUP from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import './App.css';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>);
-}
+
 
 function App() {
   console.log(Route);
@@ -22,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
 
       </Switch>
     </div>
