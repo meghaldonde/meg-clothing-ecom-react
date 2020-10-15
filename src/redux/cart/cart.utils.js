3c65309706946f12619da83ac19fcc6501dcb5b4
meghaldonde <meghal.donde@gmail.com>
2020-10-15 15:33:42 -0400

    added quanity remove or add on checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -18,3 +18,32 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         quantity: 1
     }]
 };
+
+// export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+//     const existingCartItem = cartItems.find(
+//         cartItem => cartItem.id === cartItemToRemove.id
+//     )
+
+//     if (existingCartItem.quanity === 1) {
+//         return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+//     }
+
+//     return cartItems.map(
+//         cartItem =>
+//         cartItem.id === cartItemToRemove.id ? {
+//             ...cartItem,
+//             quanity: cartItem.quanity - 1
+//         } :
+//         cartItem
+//     );
+// };
+
+export const removeItemFromCart = (cartItems, itemToRemove) =>
+    cartItems.flatMap((item) => {
+        return item.id !== itemToRemove.id ?
+            item :
+            item.quantity === 1 ? [] : {
+                ...item,
+                quantity: item.quantity - 1
+            };
+    });
